459cb3be0135daffb0a539eff93b83780a5b0684
qinxianwu <qinxianwu@qq.com>
2022-09-21 18:55:14 +0800

    fix: 清除不必要注释

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,3 @@
-// vite不能使用require
-// 解决方法 引入插件 @originjs/vite-plugin-commonjs 和 @originjs/vite-plugin-require-context
 const modulesFiles = require.context("./module/", true, /\.js$/);
 const constants = modulesFiles.keys().reduce((modules, modulePath) => {
   const value = modulesFiles(modulePath);
