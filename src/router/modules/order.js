a59253a528d553d158d2b245c51c8a877bef1970
qinxianwu <qinxianwu@qq.com>
2022-10-25 17:02:09 +0800

    feat: 配置路由ID

--- src/router/modules/order.js
+++ src/router/modules/order.js
@@ -1,3 +1,4 @@
+import PERMISSION_ID from "@/permission/PERMISSION_ID";
 import OrderRouterChildren from "../config/order";
 import MainLayout from "@/layouts/MainLayout/index";
 
@@ -9,6 +10,7 @@ export default {
     title: "订单出票",
     icon: "order",
     affix: true,
+    PermissionId: PERMISSION_ID.Order,
   },
   children: OrderRouterChildren,
 };
