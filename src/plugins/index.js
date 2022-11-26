a6b32cd807ae7ad0d320c2f101c5bbd530d684b4
qinxianwu <win2027@sina.com>
2022-11-26 17:42:58 +0800

    feat: 去除图表引入

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -6,4 +6,4 @@ import "@/plugins/element-ui.js"; // element-ui 配置
  * 原因 ThemeRiverView.js 文件的中有一个方法为 process => 和node环境中process重名
  * 解决方法 修改 ThemeRiverView.js 方法名称
  */
-import "@/plugins/echarts.js"; // echarts 配置
+// import "@/plugins/echarts.js"; // echarts 配置
